41d5726d12ee419d7c9c8899f5f4d1f9363608e6
Stefan Peters <Stefan.Peters@gbv.de>
2022-07-13 10:24:53 +0200

    Add indexes to import-subjects.sql

--- import-subjects.sql
+++ import-subjects.sql
@@ -4,6 +4,9 @@ CREATE TABLE IF NOT EXISTS subjects (
   voc TEXT NOT NULL,
   notation TEXT NOT NULL
 );
+CREATE INDEX idx_notation on subjects (notation);
+CREATE INDEX idx_ppn on subjects (ppn);
+CREATE INDEX idx_voc on subjects (voc);
 
 .mode tabs
 .import subjects.tsv subjects
